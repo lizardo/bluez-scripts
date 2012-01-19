da644241e2a3877376ec0c9f606983b65cf34ea6
Anderson Lizardo <anderson.lizardo@openbossa.org>
2012-01-19 10:27:14 -0400

    Check for Aegis mode before installing package
    The package should always be installed on Normal mode (running official
    kernel), otherwise Aegis validation will fail and device will not boot.

--- create_deb_for_n9.sh
+++ create_deb_for_n9.sh
@@ -15,4 +15,16 @@ tar -C $tmp_dir/bluez-le/lib/modules -xf kernel-modules.tar
 cp DEBIAN.control $tmp_dir/bluez-le/DEBIAN/control
 echo "Installed-Size: $(du -k -s $tmp_dir/bluez-le | awk '{print $1}')" >> $tmp_dir/bluez-le/DEBIAN/control
 
+cat > $tmp_dir/bluez-le/DEBIAN/preinst << "EOF"
+#!/bin/sh -e
+
+if ! accli -I | grep -q '^Current mode: normal'; then
+    echo "ERROR: This package should be installed on normal mode (i.e. with signed kernel)." >&2
+    exit 1
+fi
+
+exit 0
+EOF
+chmod 755 $tmp_dir/bluez-le/DEBIAN/preinst
+
 fakeroot dpkg -b $tmp_dir/bluez-le bluez-le_armel.deb
