b3b6b4dc51821785a6779478bed8aa5c4eaeb2e9
Anderson Lizardo <anderson.lizardo@openbossa.org>
2012-08-30 17:02:18 -0400

    Updates for latest compat-drivers

--- README.rst
+++ README.rst
@@ -3,11 +3,11 @@ Building BlueZ, kernel and modules for N9
 
 #. Install lzop (required for building the kernel).
 #. Setup `Harmattan Platform SDK <http://harmattan-dev.nokia.com/docs/library/html/guide/html/Developer_Library_Alternative_development_environments_Platform_SDK_user_guide_Installing_Harmattan_Platform_SDK.html>`_.
-#. Get `N9 kernel source <http://harmattan-dev.nokia.com/pool/harmattan/free/k/kernel/kernel_2.6.32-20115101+0m7.tar.gz>`_. Save the tarball onto the same directory as the "build_for_n9.sh" script.
-#. Get `wireless-compat source <http://linuxwireless.org/download/compat-wireless-2.6/compat-wireless-2012-03-18.tar.bz2>`_. Save the tarball onto the same directory as "build_for_n9.sh".
+#. Get `N9 kernel source <http://molly.corsac.net/~corsac/n9/sources-pr1.3/kernel_2.6.32-20121301+0m8.tar.gz>`_ (MD5: 32dd66fd23cf1088cc8edbafd51f5c3f; SHA1: 0757aa15e15eaedbb3bbee4d2672ba557fa60766). Save the tarball onto the same directory as the "build_for_n9.sh" script.
+#. Get `wireless-compat source <http://www.orbit-lab.org/kernel/compat-wireless-2.6/compat-wireless-2012-08-27.tar.bz2>`_. Save the tarball onto the same directory as "build_for_n9.sh".
 #. Get BlueZ tree with latest LE development::
 
-        git clone -b integration git://gitorious.org/~lizardo/bluez/lizardo-bluez.git
+        git clone -b master git://git.kernel.org/pub/scm/bluetooth/bluez.git
 
 This tree should also be on the same directory as "build_for_n9.sh".
 
@@ -39,6 +39,10 @@ on the "Transfers" window.
 
 Booting the custom kernel on N9
 ===============================
+
+NOTE: Since PR1.3, it is not possible to just boot a custom kernel without
+trigerring the warranty warning message and requiring a full re-flash to
+restore it.
 
 To boot the custom kernel (without flashing it), run::
 
--- build_for_n9.sh
+++ build_for_n9.sh
@@ -11,7 +11,7 @@ tmp_dir=$(mktemp -d)
 trap "rm -rf $tmp_dir" EXIT
 
 ksrc=$PWD/kernel-2.6.32
-test -d $ksrc || tar -xvzf $(egrep -o 'kernel_2.6.32-.*\.tar\.gz' README.rst)
+test -d $ksrc || tar -xzf $(egrep -o 'kernel_2.6.32-.*\.tar\.gz' README.rst)
 export QUILT_PATCHES=$PWD/kernel-patches
 cd $ksrc
 if quilt unapplied; then
@@ -30,8 +30,9 @@ mod_dir=$(cd $tmp_dir && ls -d lib/modules/*)
 
 # compile compat-wireless (for bluetooth backport)
 compat=$(egrep -o 'compat-wireless-[0-9-]{10}' README.rst)
-test -d $compat || tar -xvjf $compat.tar.bz2
+test -d $compat || tar -xjf $compat.tar.bz2
 export QUILT_PATCHES=$PWD/compat-wireless-patches
+test -d $compat/drivers/bluetooth/hci_h4p || cp -a $ksrc/drivers/bluetooth/hci_h4p/ $compat/drivers/bluetooth
 cd $compat
 if quilt unapplied; then
     quilt push -a
@@ -50,16 +51,15 @@ sed -r -i -e "s,^(kernel|updates)/,/$mod_dir/\1/,g" \
 # remove unnecessary symlinks
 rm $tmp_dir/$mod_dir/{build,source}
 
-fakeroot tar -C $tmp_dir/lib/modules -cvf kernel-modules.tar $(basename $mod_dir)
+fakeroot tar -C $tmp_dir/lib/modules -cf kernel-modules.tar $(basename $mod_dir)
 
-(cd bluez/ && git archive --format=tar --prefix=bluez/ HEAD) | tar -C $tmp_dir -xvf -
+(cd bluez/ && git archive --format=tar --prefix=bluez/ HEAD) | tar -C $tmp_dir -xf -
 sb-conf se HARMATTAN_ARMEL
 cat << EOF | scratchbox -s
 set -e -u
 cd $tmp_dir/bluez
 ./bootstrap-configure --prefix=/opt/bluez --sysconfdir=/opt/bluez/etc \
-    --localstatedir=/opt/bluez/var --enable-maemo6 --disable-capng \
-    --disable-maintainer-mode --with-time=timed
+    --localstatedir=/opt/bluez/var --disable-maintainer-mode
 make -j$NJOBS
 make DESTDIR=$tmp_dir/bluez-bin install
 mkdir -p $tmp_dir/bluez-bin/opt/bluez/etc/bluetooth
@@ -81,11 +81,11 @@ DebugKeys = false
 
 # GATT specific
 AutoConnectTimeout = 60
-AttributeServer = true
+EnableGatt = true
 _EOF_
 
-cp proximity/proximity.conf $tmp_dir/bluez-bin/opt/bluez/etc/bluetooth/
-cp mgmt/btmgmt $tmp_dir/bluez-bin/opt/bluez/bin
+cp profiles/proximity/proximity.conf $tmp_dir/bluez-bin/opt/bluez/etc/bluetooth/
+cp tools/btmgmt $tmp_dir/bluez-bin/opt/bluez/bin
 EOF
 
-fakeroot tar -C $tmp_dir/bluez-bin/opt -cvf bluez-bin.tar bluez
+fakeroot tar -C $tmp_dir/bluez-bin/opt -cf bluez-bin.tar bluez
